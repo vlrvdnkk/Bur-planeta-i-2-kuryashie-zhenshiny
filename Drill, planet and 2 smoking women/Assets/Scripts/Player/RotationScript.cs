66c91315ac6d40d1b8347d8b85b45e8c87092e87
vlrvdnkk <krasilnikovaaa21@st.ithub.ru>
2023-11-08 18:41:45 +0300

    Changed core mechanic of main cannon

--- Drill, planet and 2 smoking women/Assets/Scripts/Player/RotationScript.cs
+++ Drill, planet and 2 smoking women/Assets/Scripts/Player/RotationScript.cs
@@ -2,37 +2,34 @@ using UnityEngine;
 
 public class RotationScript : MonoBehaviour
 {
-    [SerializeField] private float a = 1f; // Полуось по горизонтали (мажорная полуось)
-    [SerializeField] private float b = 3f; // Полуось по вертикали (минорная полуось);
-    [SerializeField] private float maxRotationSpeed = 575f; // Максимальная скорость вращения
-
-    private Vector3 targetPosition;
-    private float currentAngle;
+    public SpriteRenderer spriteRenderer; // Ссылка на компонент SpriteRenderer объекта
+    public Sprite normalSprite; // Обычный спрайт
+    public Sprite mirroredSprite; // Зеркальный спрайт
 
     void Update()
     {
-        Vector3 worldPos = Input.mousePosition;
-        worldPos = Camera.main.ScreenToWorldPoint(new Vector3(worldPos.x, worldPos.y, 10f)); // Поднимаем Z, чтобы объект находился перед плоскостью
-
-        // Находим ближайшую точку на границе эллипса к позиции курсора
-        Vector3 nearestPoint = FindNearestPointOnEllipse(worldPos);
-
-        // Поворачиваем объект так, чтобы он смотрел на позицию курсора
-        Vector3 lookPos = worldPos - nearestPoint;
-        float targetAngle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg - 90f; // Вычитаем 90 градусов, чтобы смотреть прямо
-        currentAngle = Mathf.MoveTowardsAngle(currentAngle, targetAngle, maxRotationSpeed * Time.deltaTime);
-
-        // Устанавливаем новую позицию и вращение объекта
-        transform.position = nearestPoint;
-        transform.rotation = Quaternion.Euler(0, 0, currentAngle);
+        Rotation();
     }
 
-    Vector3 FindNearestPointOnEllipse(Vector3 target)
+    void Rotation()
     {
-        float angle = Mathf.Atan2(target.y, target.x);
-        float x = a * Mathf.Cos(angle);
-        float y = b * Mathf.Sin(angle);
-        return new Vector3(x, y, transform.position.z);
+        Vector3 worldPos = Input.mousePosition;
+        worldPos = Camera.main.ScreenToWorldPoint(worldPos);
+
+        // Вычисляем угол между текущей позицией объекта и позицией мыши
+        float angle = Mathf.Atan2(worldPos.y - transform.position.y, worldPos.x - transform.position.x) * Mathf.Rad2Deg;
+
+        // Вращаем объект вокруг оси Z на 360 градусов
+        transform.rotation = Quaternion.Euler(0, 0, angle - 90);
+
+        // В зависимости от угла, выбираем спрайт
+        if ((angle >= 0 && angle < 90) || (angle >= -90 && angle < 0))
+        {
+            spriteRenderer.sprite = mirroredSprite; // Зеркальный спрайт
+        }
+        else
+        {
+            spriteRenderer.sprite = normalSprite; // Обычный спрайт
+        }
     }
 }
-
